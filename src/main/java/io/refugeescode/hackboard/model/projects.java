b81c1d5641462355808a20d3f1e9b89a2f3ae98b
Thaerohde <thaerohde@gmail.com>
2018-04-26 15:33:46 +0200

    add projectentity

---
+++ src/main/java/io/refugeescode/hackboard/model/projects.java
@@ -0,0 +1,53 @@
+package io.refugeescode.hackboard.components;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class projects {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String title;
+    private String description;
+    @Override
+    public String toString() {
+        return "projects{" +
+            "id=" + id +
+            ", title='" + title + '\'' +
+            ", description='" + description + '\'' +
+            '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+
+
+
+}
