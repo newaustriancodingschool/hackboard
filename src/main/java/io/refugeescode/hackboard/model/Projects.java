117252f38445bac026b987b4da3679bc3606b292
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2018-04-28 15:54:28 +0200

    resolved merge conflicts.

--- src/main/java/io/refugeescode/hackboard/model/Projects.java
+++ src/main/java/io/refugeescode/hackboard/model/Projects.java
@@ -16,20 +16,13 @@ import java.io.Serializable;
 
 @Entity
 public class Projects {
-
-
     @Id
-<<<<<<< HEAD
-    @GeneratedValue
-    private Long id;
-=======
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
     @Size(min = 1, max = 100)
     @Column(length = 100, nullable = false)
->>>>>>> 5c149daa549654040395e8a33be70c9bdd6fdbc5
     private String title;
 
     @NotNull
@@ -40,10 +33,6 @@ public class Projects {
     public Projects() {
     }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 5c149daa549654040395e8a33be70c9bdd6fdbc5
     public Long getId() {
         return id;
     }
--- src/main/java/io/refugeescode/hackboard/repository/ProjectsRepository.java
+++ src/main/java/io/refugeescode/hackboard/repository/ProjectsRepository.java
@@ -4,15 +4,6 @@ import io.refugeescode.hackboard.model.Projects;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface ProjectsRepository extends JpaRepository <Projects,Long>{
-    Optional<Projects> findOneByTitleIgnoreCase(String title);
-
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 5c149daa549654040395e8a33be70c9bdd6fdbc5
+public interface ProjectsRepository extends JpaRepository <Projects, Long> {
 }
