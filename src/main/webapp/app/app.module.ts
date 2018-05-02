df55bb2dc60fae5e6674dc4e7360028913ee0a00
farah7tawfeek <farahalrubaye7@gmail.com>
2018-05-02 14:47:48 +0200

    #1:editing and #3:copmonenet created

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -27,6 +27,7 @@ import {
     ActiveMenuDirective,
     ErrorComponent
 } from './layouts';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 @NgModule({
     imports: [
@@ -46,7 +47,8 @@ import {
         ErrorComponent,
         PageRibbonComponent,
         ActiveMenuDirective,
-        FooterComponent
+        FooterComponent,
+        ProjectListComponent
     ],
     providers: [
         ProfileService,
---
+++ src/main/webapp/app/project-list/project-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-project-list',
+  templateUrl: './project-list.component.html',
+  styles: []
+})
+export class ProjectListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
