f6baa6229a1e0ffbaa359c057a4d819af41c1ac3
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2018-05-03 20:42:38 +0200

    #19: test run for prettier

--- src/main/webapp/app/app.main.ts
+++ src/main/webapp/app/app.main.ts
@@ -1,13 +1,11 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { ProdConfig } from './blocks/config/prod.config';
-import { HackboardAppModule } from './app.module';
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { ProdConfig } from "./blocks/config/prod.config";
+import { HackboardAppModule } from "./app.module";
 
 ProdConfig();
 
-if (module['hot']) {
-    module['hot'].accept();
+if (module["hot"]) {
+    module["hot"].accept();
 }
 
-platformBrowserDynamic().bootstrapModule(HackboardAppModule)
-.then((success) => console.log(`Application started`))
-.catch((err) => console.error(err));
+platformBrowserDynamic().bootstrapModule(HackboardAppModule);
