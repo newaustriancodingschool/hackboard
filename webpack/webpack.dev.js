07a7e66d99187c2b32f9d099ab746eebcb422abd
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2018-05-08 11:02:36 +0200

    #2: Using logo instead of jhipster for webpack build process

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -87,7 +87,7 @@ module.exports = webpackMerge(commonConfig({ env: ENV }), {
     new webpack.WatchIgnorePlugin([utils.root('src/test')]),
     new WebpackNotifierPlugin({
       title: 'HackBoard',
-      contentImage: path.join(__dirname, 'logo-jhipster.png')
+      contentImage: path.join(__dirname, 'logo-hackboard.png')
     })
   ]
 });
