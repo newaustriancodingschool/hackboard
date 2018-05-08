8b00e6102bcb631b2ceeb0f14eb1dda46df58b1c
mohammadsalkini <eng.salkini@hotmail.com>
2018-05-08 13:30:37 +0200

    #30: Add security annotaions

--- src/main/java/io/refugeescode/hackboard/controller/ProjectsController.java
+++ src/main/java/io/refugeescode/hackboard/controller/ProjectsController.java
@@ -3,6 +3,7 @@ package io.refugeescode.hackboard.controller;
 
 import io.refugeescode.hackboard.domain.Project;
 import io.refugeescode.hackboard.repository.ProjectRepository;
+import io.refugeescode.hackboard.security.AuthoritiesConstants;
 import io.refugeescode.hackboard.service.dto.ProjectDto;
 import io.refugeescode.hackboard.service.mapper.ProjectMapper;
 import io.refugeescode.hackboard.web.api.controller.ProjectsApi;
@@ -10,6 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +26,8 @@ public class ProjectsController implements ProjectsApi {
     }
 
     @Override
-    public ResponseEntity<Boolean> addProject(@RequestBody ProjectDto project) {
+    @Secured({AuthoritiesConstants.USER, AuthoritiesConstants.ADMIN})
+    public ResponseEntity<Boolean> addProject(ProjectDto project) {
         Project entity = new Project();
         entity.setTitle(project.getTitle());
         entity.setDescription(project.getDescription());
@@ -35,7 +38,8 @@ public class ProjectsController implements ProjectsApi {
     }
 
     @Override
-    public ResponseEntity<Boolean> editProject(@RequestBody ProjectDto project) {
+    @Secured({AuthoritiesConstants.USER, AuthoritiesConstants.ADMIN})
+    public ResponseEntity<Boolean> editProject(ProjectDto project) {
         Project entity = projectsRepository.findOne(project.getId());
         entity.setTitle(project.getTitle());
         entity.setDescription(project.getDescription());
@@ -46,6 +50,7 @@ public class ProjectsController implements ProjectsApi {
     }
 
     @Override
+    @Secured(AuthoritiesConstants.ANONYMOUS)
     public ResponseEntity<List<ProjectDto>> listProjects() {
         return new ResponseEntity<>(
             projectsRepository.findAll().stream()
@@ -56,6 +61,7 @@ public class ProjectsController implements ProjectsApi {
     }
 
     @Override
+    @Secured(AuthoritiesConstants.ANONYMOUS)
     public ResponseEntity<ProjectDto> viewProject(@PathVariable("projectId") Long projectId) {
         return new ResponseEntity<>(
             ProjectMapper.INSTANCE.projectToProjectDto(
@@ -65,6 +71,7 @@ public class ProjectsController implements ProjectsApi {
     }
 
     @Override
+    @Secured({AuthoritiesConstants.USER, AuthoritiesConstants.ADMIN})
     public ResponseEntity<Boolean> deleteProject(@PathVariable("projectId") Long projectId) {
         projectsRepository.delete(projectId);
         return new ResponseEntity<>(true, HttpStatus.OK);
