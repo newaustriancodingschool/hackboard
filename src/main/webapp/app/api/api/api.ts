34e6a14ed16b5fb6e474408cf92c99d57fd65525
MohamedDahman <mhd.dahman@yahoo.com>
2018-05-13 21:39:35 +0200

    #29: Project Competencies

--- src/main/webapp/app/api/api/api.ts
+++ src/main/webapp/app/api/api/api.ts
@@ -1,3 +1,5 @@
+export * from './competency.service';
+import { CompetencyService } from './competency.service';
 export * from './project.service';
 import { ProjectService } from './project.service';
-export const APIS = [ProjectService];
+export const APIS = [CompetencyService, ProjectService];
--- src/main/webapp/app/api/model/models.ts
+++ src/main/webapp/app/api/model/models.ts
@@ -1,1 +1,2 @@
+export * from './competencyDto';
 export * from './projectDto';
