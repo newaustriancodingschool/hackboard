e393a426e8f96b58af39e9e44909175f944f5895
farah7tawfeek <farahalrubaye7@gmail.com>
2018-05-14 16:59:43 +0200

    #6: delete feature finished

--- src/main/webapp/app/projects/view/view.component.ts
+++ src/main/webapp/app/projects/view/view.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectDto, ProjectService } from '../../api';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   templateUrl: './view.component.html',
@@ -9,12 +9,21 @@ import { ActivatedRoute } from '@angular/router';
 export class ProjectViewComponent implements OnInit {
   data: ProjectDto = { id: 0, title: '', description: '' };
 
-  constructor(private projectService: ProjectService, private route: ActivatedRoute) {}
+  constructor(
+    private projectService: ProjectService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     const id = parseInt(this.route.snapshot.paramMap.get('id'), 10);
     this.projectService.viewProject(id).subscribe(project => {
       this.data = project;
     });
+  }
+  delete() {
+    this.projectService
+      .deleteProject(this.data.id)
+      .subscribe(() => this.router.navigate(['/projects']));
   }
 }
