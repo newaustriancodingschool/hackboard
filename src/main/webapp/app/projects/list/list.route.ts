c1ad3179b20ffb79091f064e34a667c548de9297
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2018-05-14 23:58:16 +0200

    #3: fixed build

--- src/main/webapp/app/projects/list/list.route.ts
+++ src/main/webapp/app/projects/list/list.route.ts
@@ -1,6 +1,4 @@
 import { Route } from '@angular/router';
-
-import { UserRouteAccessService } from '../../shared';
 import { ProjectListComponent } from './list.component';
 
 export const projectListRoute: Route = {
