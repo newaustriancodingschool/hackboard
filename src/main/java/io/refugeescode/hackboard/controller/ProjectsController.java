437542087f1efa421af64669902081b5be99ecaa
mohammadsalkini <eng.salkini@hotmail.com>
2018-05-15 13:33:52 +0200

    #30: Add security to endpoint

--- src/main/java/io/refugeescode/hackboard/controller/ProjectsController.java
+++ src/main/java/io/refugeescode/hackboard/controller/ProjectsController.java
@@ -5,6 +5,7 @@ import io.refugeescode.hackboard.domain.Project;
 import io.refugeescode.hackboard.domain.User;
 import io.refugeescode.hackboard.repository.ProjectRepository;
 import io.refugeescode.hackboard.repository.UserRepository;
+import io.refugeescode.hackboard.security.AuthoritiesConstants;
 import io.refugeescode.hackboard.security.SecurityUtils;
 import io.refugeescode.hackboard.service.dto.ProjectDto;
 import io.refugeescode.hackboard.service.mapper.ProjectMapper;
@@ -12,13 +13,21 @@ import io.refugeescode.hackboard.web.api.controller.ProjectsApi;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
+import io.refugeescode.hackboard.web.rest.util.HeaderUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.Entity;
 import javax.validation.Valid;
+
+import static io.refugeescode.hackboard.security.AuthoritiesConstants.ADMIN;
+import static io.refugeescode.hackboard.security.AuthoritiesConstants.USER;
+import static org.hibernate.id.IdentifierGenerator.ENTITY_NAME;
 
 @RestController
 public class ProjectsController implements ProjectsApi {
@@ -35,6 +44,9 @@ public class ProjectsController implements ProjectsApi {
     @Override
 //    @Secured({AuthoritiesConstants.USER, AuthoritiesConstants.ADMIN})
     public ResponseEntity<Boolean> addProject(@RequestBody ProjectDto project) {
+        if (!SecurityUtils.isCurrentUserInRole(ADMIN)) {
+            return ResponseEntity.badRequest().header(String.valueOf(HeaderUtil.createFailureAlert(ENTITY_NAME, "Not authenticated", "You need to be n admin to perform this action " ))).body(null);
+        }
         Project entity = new Project();
         entity.setTitle(project.getTitle());
         entity.setDescription(project.getDescription());
@@ -52,6 +64,10 @@ public class ProjectsController implements ProjectsApi {
     @Override
 //    @Secured({AuthoritiesConstants.USER, AuthoritiesConstants.ADMIN})
     public ResponseEntity<Boolean> editProject(@RequestBody ProjectDto project) {
+        if (!SecurityUtils.isCurrentUserInRole(ADMIN) || !SecurityUtils.isCurrentUserInRole(USER)) {
+            return ResponseEntity.badRequest().header(String.valueOf(HeaderUtil.createFailureAlert(ENTITY_NAME, "Not authenticated", "You need to be n admin to perform this action " ))).body(null);
+        }
+
         Project entity = projectsRepository.findOne(project.getId());
         entity.setTitle(project.getTitle());
         entity.setDescription(project.getDescription());
@@ -61,8 +77,11 @@ public class ProjectsController implements ProjectsApi {
     }
 
     @Override
-//    @Secured(AuthoritiesConstants.ANONYMOUS)
+   // @Secured(AuthoritiesConstants.ADMIN)
     public ResponseEntity<List<ProjectDto>> listProjects() {
+        if (!SecurityUtils.isCurrentUserInRole(ADMIN) || !SecurityUtils.isCurrentUserInRole(USER)) {
+            return ResponseEntity.badRequest().header(String.valueOf(HeaderUtil.createFailureAlert(ENTITY_NAME, "Not authenticated", "You need to be n admin to perform this action " ))).body(null);
+        }
         return new ResponseEntity<>(
             projectsRepository.findAll().stream()
                 .map(ProjectMapper.INSTANCE::projectToProjectDto)
@@ -75,6 +94,9 @@ public class ProjectsController implements ProjectsApi {
 //    @Secured(AuthoritiesConstants.ANONYMOUS)
     public ResponseEntity<ProjectDto> viewProject(@PathVariable("projectId") Long projectId) {
 
+        if (!SecurityUtils.isCurrentUserInRole(ADMIN) || !SecurityUtils.isCurrentUserInRole(USER)) {
+            return ResponseEntity.badRequest().header(String.valueOf(HeaderUtil.createFailureAlert(ENTITY_NAME, "Not authenticated", "You need to be n admin to perform this action " ))).body(null);
+        }
         return new ResponseEntity<>(
             ProjectMapper.INSTANCE.projectToProjectDto(
                 projectsRepository.findOne(projectId)),
