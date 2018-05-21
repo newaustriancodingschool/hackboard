06abb35b6945b725bd4b88590f4aef3947fed614
MohamedDahman <mhd.dahman@yahoo.com>
2018-05-21 10:57:57 +0200

    #70: Remove code for competency

--- src/main/java/io/refugeescode/hackboard/config/CacheConfiguration.java
+++ src/main/java/io/refugeescode/hackboard/config/CacheConfiguration.java
@@ -45,7 +45,6 @@ public class CacheConfiguration {
             cm.createCache(io.refugeescode.hackboard.domain.PersistentToken.class.getName(), jcacheConfiguration);
             cm.createCache(io.refugeescode.hackboard.domain.User.class.getName() + ".persistentTokens", jcacheConfiguration);
             cm.createCache(io.refugeescode.hackboard.domain.Project.class.getName(), jcacheConfiguration);
-            cm.createCache(io.refugeescode.hackboard.domain.Competency.class.getName(),jcacheConfiguration);
             cm.createCache(io.refugeescode.hackboard.domain.ProjectRole.class.getName(),jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
--- src/main/webapp/app/api/api/api.ts
+++ src/main/webapp/app/api/api/api.ts
@@ -1,7 +1,5 @@
-export * from './competency.service';
-import { CompetencyService } from './competency.service';
 export * from './project.service';
 import { ProjectService } from './project.service';
 export * from './projectRole.service';
 import { ProjectRoleService } from './projectRole.service';
-export const APIS = [CompetencyService, ProjectService, ProjectRoleService];
+export const APIS = [ProjectService, ProjectRoleService];
