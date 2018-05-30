ee4c3085d03575930891a182cbbc684c1311d4dc
farah7tawfeek <farahalrubaye7@gmail.com>
2018-05-30 16:34:03 +0200

    #89: Application button service worked

--- src/main/webapp/app/projects/project-view/project-view.component.ts
+++ src/main/webapp/app/projects/project-view/project-view.component.ts
@@ -1,3 +1,6 @@
+import { ApplicationDto } from './../../api/model/applicationDto';
+import { ApplicationService } from './../../api/api/application.service';
+import { ApplicantService } from './../../api/api/applicant.service';
 import { Component, OnInit } from '@angular/core';
 import { ProjectDto, ProjectService } from '../../api';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -7,8 +10,10 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ProjectViewComponent implements OnInit {
   project: ProjectDto = { id: 0, title: '', description: '' };
+  applicant: ApplicationDto = { applicant: 0, projectId: 0, roleId: 0 };
   constructor(
     private projectService: ProjectService,
+    private applicationService: ApplicationService,
     private route: ActivatedRoute,
     private router: Router
   ) {}
@@ -25,6 +30,14 @@ export class ProjectViewComponent implements OnInit {
   delete() {
     this.projectService
       .deleteProject(this.project.id)
+      .subscribe(() => this.router.navigate(['/projects']));
+  }
+  apply() {
+    this.applicant.projectId = 5;
+    this.applicant.roleId = 4;
+    this.applicant.applicant = 3;
+    this.applicationService
+      .addapplication(this.applicant)
       .subscribe(() => this.router.navigate(['/projects']));
   }
 }
