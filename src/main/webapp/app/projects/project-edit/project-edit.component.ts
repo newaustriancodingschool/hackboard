09ae25fa06a3a545f6e89f041c71de45a89fe673
HosamAlmoghraby <32025281+HosamAlmoghraby@users.noreply.github.com>
2018-06-01 23:05:45 +0200

    #66: show project roles in edit page

--- src/main/webapp/app/projects/project-edit/project-edit.component.ts
+++ src/main/webapp/app/projects/project-edit/project-edit.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectDto, ProjectService } from '../../api';
 import { ActivatedRoute, Router } from '@angular/router';
+import { ProjectRoleService } from './../../api/api/projectRole.service';
+import { ProjectRoleDto } from './../../api/model/projectRoleDto';
 
 @Component({
   templateUrl: './project-edit.component.html',
@@ -8,21 +10,65 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class ProjectEditComponent implements OnInit {
   data: ProjectDto = { id: 0, title: '', description: '', github: '' };
+  roles: Array<ProjectRoleDto>;
+  projectRoles: Array<ProjectRoleDto>;
 
   constructor(
     private projectService: ProjectService,
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    private projectRoleService: ProjectRoleService
   ) {}
 
   ngOnInit() {
     const id = parseInt(this.route.snapshot.paramMap.get('id'), 10);
     this.projectService.viewProject(id).subscribe(project => {
       this.data = project;
+      this.projectRoleService.listProjectRoles().subscribe(roles => (this.roles = roles));
+      this.projectRoles = [];
     });
   }
 
   submit() {
     this.projectService.editProject(this.data).subscribe(() => this.router.navigate(['/projects']));
+  }
+
+  editRole(roleValue) {
+    const newRoleData = { roleName: '', color: '', count: 1 };
+    newRoleData.roleName = roleValue;
+    let tmpcolor = '';
+    for (let i = 0; i < this.roles.length; i++) {
+      if (this.roles[i].roleName === roleValue) {
+        tmpcolor = this.roles[i].color;
+      }
+    }
+    newRoleData.color = tmpcolor;
+    let roleFound = false;
+    for (let i = 0; i < this.projectRoles.length; i++) {
+      if (roleValue === this.projectRoles[i].roleName) {
+        this.projectRoles[i].count++;
+        roleFound = true;
+      }
+    }
+    if (roleFound === false) {
+      this.projectRoles.push(newRoleData);
+    }
+  }
+
+  deleteRole(role) {
+    for (let i = 0; i < this.projectRoles.length; i++) {
+      console.log(this.projectRoles[i].roleName);
+      if (role === this.projectRoles[i].roleName) {
+        if (this.projectRoles[i].count === 1) {
+          this.projectRoles.splice(i, 1);
+        } else {
+          this.projectRoles[i].count--;
+        }
+      }
+    }
+  }
+
+  getFilledArray(count) {
+    return Array(count).fill(true);
   }
 }
