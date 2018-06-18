84f1b3c32b43418a2ed8a806b6c86f0e05da3035
farah7tawfeek <farahalrubaye7@gmail.com>
2018-06-18 12:20:49 +0200

    #45: pulled the last version

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
-
 import { Account, LoginModalService, Principal } from '../shared';
 
 @Component({
